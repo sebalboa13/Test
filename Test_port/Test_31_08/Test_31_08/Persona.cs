8fe4187b186b36ab1357a6399cf87ab7362832d5
sebalboa13 <sebastianbalboa13@gmail.com>
2021-08-31 21:32:00 -0400

    test 1

---
+++ Test_port/Test_31_08/Test_31_08/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_31_08
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public Double Estatura { get; set; }
+
+        public Persona()
+        {
+            this init();
+        }
+
+        private void init()
+        {
+            Nombre = string.Empty;
+            Apellido = string.Empty;
+            FechaNacimiento = DateTime.Today;
+            Estatura = 0;
+        }
+    }
+}
